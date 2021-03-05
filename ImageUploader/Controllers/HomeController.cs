1a8e3fedba566ca3099965e05b919c7e39043a4f
andyhogg1972 <andyhogg@gmail.com>
2021-03-05 13:06:38 +0000

    Test harness for ImageFileResizer

---
+++ ImageUploader/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using ImageUploader.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            ProfileViewModel model = new ProfileViewModel();
+            model.FileInfos = new DirectoryInfo(Server.MapPath("~/images")).GetFiles();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase profileFile)
+        {
+            if (profileFile != null)
+            {
+                string pic = System.IO.Path.GetFileName(profileFile.FileName);
+                string path = System.IO.Path.Combine(Server.MapPath("~/images"), pic);
+                profileFile.SaveAs(path);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ImageUploader/Models/ProfileViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace ImageUploader.Models
+{
+    public class ProfileViewModel
+    {
+        public string ProfileImage
+        {
+            get;
+            set;
+        }
+
+        public FileInfo[] FileInfos
+        {
+            get; set;
+        }
+    }
+}
